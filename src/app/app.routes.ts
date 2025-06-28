b42bd9d7a19bd05413fb4bc7910c3b3ece307a8c
Evgen <pipkoevgeniy@gmail.com>
2025-06-28 16:07:42 +0200

    structure created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PracticeComponent } from './pages/practice/practice.component.';
+import { TeamComponent } from './pages/team/team.component.';
+import { ContactComponent } from './pages/contact/contact.component.';
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Головна'
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    title: 'Про компанію'
+  },
+  {
+    path: 'practice',
+    component: PracticeComponent,
+    title: 'Практика'
+  },
+  {
+    path: 'team',
+    component: TeamComponent,
+    title: 'Команда'
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    title: 'Контакти'
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './shared/header/header.component';
+import {FooterComponent} from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
   protected title = 'dap';
