37988aec2636120ce62ece3d01e9371deccd9ef1
Evgen <pipkoevgeniy@gmail.com>
2025-06-29 18:32:23 +0200

    breadcrumbs created

--- src/app/pages/services/legal/cases/family/divorce/divorce.component.ts
+++ src/app/pages/services/legal/cases/family/divorce/divorce.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterLink} from '@angular/router';
+import {BreadcrumbsComponent} from '../../../../../../shared/breadcrumbs/breadcrumbs.component';
 
 @Component({
   selector: 'app-divorce.component',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, BreadcrumbsComponent],
   templateUrl: './divorce.component.html',
   styleUrl: './divorce.component.scss'
 })
