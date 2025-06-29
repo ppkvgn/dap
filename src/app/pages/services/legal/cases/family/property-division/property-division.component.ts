8ecdabccf96e3e33842713bf2b04b1cd724f0a8f
Evgen <pipkoevgeniy@gmail.com>
2025-06-29 18:33:44 +0200

    breadcrumbs added to family child

--- src/app/pages/services/legal/cases/family/property-division/property-division.component.ts
+++ src/app/pages/services/legal/cases/family/property-division/property-division.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {BreadcrumbsComponent} from '../../../../../../shared/breadcrumbs/breadcrumbs.component';
+import {RouterLink} from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-property-division',
-  imports: [CommonModule],
+  imports: [CommonModule, BreadcrumbsComponent, RouterLink],
   templateUrl: './property-division.component.html',
 })
 export class PropertyDivisionComponent {}
