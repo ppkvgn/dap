bb18d9ca8c4de6002f2163c97b79e77993ecaad5
Evgen <pipkoevgeniy@gmail.com>
2025-07-01 22:14:14 +0200

    search added to web and mobile, need to add pages

--- src/app/data/legal-search-data.ts
+++ src/app/data/legal-search-data.ts
@@ -7,10 +7,22 @@ export const legalSearchData = [
     description: 'Стягнення аліментів через суд або за домовленістю.'
   },
   {
+    title: 'Місце проживання дитини',
+    path: '/services/legal/family/child-residence',
+    keywords: ['місце', 'дитина', 'проживання', 'батьківство', 'підтримка'],
+    description: 'Визначення місця проживання дитини.'
+  },
+  {
     title: 'Розлучення',
     path: '/services/legal/family/divorce',
     keywords: ['розлучення', 'шлюб', 'суд'],
     description: 'Юридичний супровід припинення шлюбу.'
+  },
+  {
+    title: 'Позбавлення батьківських прав',
+    path: '/services/legal/family/parental-rights-termination',
+    keywords: ['батьківство', 'права', 'позбавлення'],
+    description: 'Позбавлення батьківських прав'
   },
   {
     title: 'Оскарження мобілізації',
