590ce8eb490e268e5612a765d5016164c2f647f3
Evgen <pipkoevgeniy@gmail.com>
2025-07-01 22:31:55 +0200

    search added to web and mobile, need to add pages

--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-team',
   imports: [],
-  templateUrl: './team.component..html',
-  styleUrl: './team.component..scss'
+  templateUrl: './team.component.html',
+  styleUrl: './team.component.scss'
 })
 export class TeamComponent {
 
