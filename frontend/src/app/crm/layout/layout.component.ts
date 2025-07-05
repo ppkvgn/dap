ef28806aceacf58307ee8bafc9c3ab54a110b76b
Evgen <pipkoevgeniy@gmail.com>
2025-07-05 12:48:41 +0200

    backend auth done

--- frontend/src/app/crm/layout/layout.component.ts
+++ frontend/src/app/crm/layout/layout.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-layout.component',
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule],
   template:  './crm-layout.component.html'
 })
 export class LayoutComponent {
